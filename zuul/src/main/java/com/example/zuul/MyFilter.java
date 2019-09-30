5e65591adac9355807fb010a698da3cd3ef570c7
zhuhaipeng <dell@qq.com>
2019-09-30 15:11:59 +0800

    pudate zull

---
+++ zuul/src/main/java/com/example/zuul/MyFilter.java
@@ -0,0 +1,37 @@
+package com.example.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext ctxt = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctxt.getRequest();
+        Object token = request.getParameter("token");
+        System.out.println("======zuul filer request token ==>"+token);
+
+        return null;
+    }
+}
